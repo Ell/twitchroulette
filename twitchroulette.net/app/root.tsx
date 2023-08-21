09f04998702445c32bd53c01824d4b805d15d11f
ellg <alan@cattes.us>
2023-08-20 22:17:24 -0500

    Add error boundry

--- twitchroulette.net/app/root.tsx
+++ twitchroulette.net/app/root.tsx
@@ -36,3 +36,14 @@ export default function App() {
     </html>
   );
 }
+
+export function ErrorBoundary({ error }) {
+  return (
+    <div>
+      <h1>Error</h1>
+      <p>{error.message}</p>
+      <p>The stack trace is:</p>
+      <pre>{error.stack}</pre>
+    </div>
+  );
+}
